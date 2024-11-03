b5a650f275f1a1b579b84fd4e4635bdb46be71ca
Roman Kuraev <naoru.desu@gmail.com>
2024-11-03 18:24:05 +0000

    feat: add logging

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import dotenv from "dotenv";
 import { createWriteStream, unlink } from "node:fs";
 import { tmpdir } from "node:os";
 import { join } from "node:path";
-
 import { patterns } from "./patterns";
 
 dotenv.config();
@@ -19,6 +18,10 @@ if (!token) {
   process.exit(1);
 }
 
+const formatLog = (msg: TelegramBot.Message, text: string) => {
+  return `[User: ${msg.from?.id}, Chat: ${msg.chat.id}] ${text}`;
+};
+
 let bot: TelegramBot;
 try {
   bot = new TelegramBot(token, { polling: true });
@@ -29,16 +32,27 @@ try {
 
 const ytDlpWrap = new YTDlpWrap("./yt-dlp");
 
+const execStreamWithLogging = async (
+  msg: TelegramBot.Message,
+  args: string[]
+): Promise<NodeJS.ReadableStream> => {
+  console.log(formatLog(msg, `Launching yt-dlp with args: ${args.join(" ")}`));
+
+  return ytDlpWrap.execStream(args);
+};
+
 bot.on("message", async (msg) => {
   for (const pattern of patterns) {
     const match = msg.text?.match(pattern.regex);
 
     if (match) {
       const url = match[0];
+      console.log(formatLog(msg, `Matched regex for URL: ${url}`));
 
       try {
         let notifyMsg: TelegramBot.Message | undefined;
 
+        console.log(formatLog(msg, `Fetching metadata for URL: ${url}`));
         const metadata = await ytDlpWrap.getVideoInfo(url);
 
         const formattedMetadata = pattern.formatMetadata
@@ -53,15 +67,19 @@ bot.on("message", async (msg) => {
         }
 
         const downloadArgs = [url, ...pattern.flags];
-
-        const stream = await ytDlpWrap.execStream(downloadArgs);
+        const stream = await execStreamWithLogging(msg, downloadArgs);
 
         const tempFilePath = join(tmpdir(), `download-${Date.now()}.mp4`);
+        console.log(
+          formatLog(msg, `Writing to temporary file: ${tempFilePath}`)
+        );
 
         const fileStream = createWriteStream(tempFilePath);
         stream.pipe(fileStream);
 
         fileStream.on("finish", async () => {
+          console.log(formatLog(msg, `Download completed, sending video`));
+
           await bot.sendVideo(msg.chat.id, tempFilePath, {
             caption: `${formattedMetadata}`,
             reply_to_message_id: msg.message_id,
@@ -72,7 +90,13 @@ bot.on("message", async (msg) => {
           }
 
           unlink(tempFilePath, (err) => {
-            if (err) console.error(`Error deleting file: ${err}`);
+            if (err) {
+              console.error(formatLog(msg, `Error deleting file: ${err}`));
+            } else {
+              console.log(
+                formatLog(msg, `Temporary file deleted: ${tempFilePath}`)
+              );
+            }
           });
         });
       } catch (error: any) {
@@ -80,7 +104,7 @@ bot.on("message", async (msg) => {
           await bot.sendMessage(msg.chat.id, `Error processing your request.`);
         }
 
-        console.error(error);
+        console.error(formatLog(msg, `Error: ${error}`));
       }
       break;
     }
